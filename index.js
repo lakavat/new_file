69103cada2dea89288dca2e6a696c9d43416f67e
raju <lakavathrajkumar90@gmail.com>
2018-10-06 18:35:02 +0530

    my_file

---
+++ index.js
@@ -0,0 +1,12 @@
+// var x="hello raj" ;
+// var y="hi raj"
+// function _name(){
+//     if (x=="hello raj" || y=="hi raj"){
+//         alert("your details matched")
+
+//     }else{
+//         alert("your details mismatched")
+//     }
+// }
+// prompt(_name())
+
---
+++ js1.js
@@ -0,0 +1,15 @@
+
+function getValue(){
+    
+    document.getElementById("demo").innerHTML = 5+9;
+}
+ function myFunction(){
+     
+     document.getElementById('my_file').innerHTML='THIS IS FAVORITE COLOR OF MINE'
+ }
+ document.write(5+4+6)
+ 
+function multiply(num1,num2 ){
+    var num=num1 * num2;
+    return result;
+}
